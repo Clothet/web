e435af737c7bcd5d71f8c37b82b15b42de50f476
Wilson Li <b84330808@gmail.com>
2016-11-20 00:47:02 +0800

    cut panel

--- src/routes/Home/components/HomeView.jsx
+++ src/routes/Home/components/HomeView.jsx
@@ -61,7 +61,7 @@ export default class HomeView extends React.Component {
                     <Tab>其他</Tab>
                   </TabList>
                   <TabPanel>
-                    <h2>Hello from Foo</h2>
+                    <p>Hello from Foo</p>
                   </TabPanel>
                   <TabPanel>
                     <h2>Hello from Bar</h2>
@@ -69,9 +69,15 @@ export default class HomeView extends React.Component {
                   <TabPanel>
                     <h2>Hello from Baz</h2>
                   </TabPanel>
+                  <TabPanel>
+                    <h2>Hello from Baz</h2>
+                  </TabPanel>
+                  <TabPanel>
+                    <h2>Hello from Baz</h2>
+                  </TabPanel>
                 </Tabs>
-              </div>
 
+              </div>
 
             </div>
 
@@ -80,7 +86,40 @@ export default class HomeView extends React.Component {
             <div className="block">
               <div className="title">個人服飾</div>
               <hr ></hr>
+              <div className="tab-area">
+                <Tabs
+                  onSelect={this.handleSelect}
+                  selectedIndex={0}
+                >
+                  <TabList>
+                    <Tab>T-Shirt & 上衣</Tab>
+                    <Tab>針織衫 & 襯衫</Tab>
+                    <Tab>外套 & 大衣</Tab>
+                    <Tab>褲裝 & 裙裝</Tab>
+                    <Tab>其他</Tab>
+                  </TabList>
+                  <TabPanel>
+                    <p>Hello from Foo</p>
+                  </TabPanel>
+                  <TabPanel>
+                    <h2>Hello from Bar</h2>
+                  </TabPanel>
+                  <TabPanel>
+                    <h2>Hello from Baz</h2>
+                  </TabPanel>
+                  <TabPanel>
+                    <h2>Hello from Baz</h2>
+                  </TabPanel>
+                  <TabPanel>
+                    <h2>Hello from Baz</h2>
+                  </TabPanel>
+                </Tabs>
+
+              </div>
+
             </div>
+
+
 
 
           </div>
@@ -91,18 +130,32 @@ export default class HomeView extends React.Component {
 
             <div className="block">
               <div className="row">
-                <div className="col-sm-4 col-xs-12">
-                  <img src={sampleImage}/>
+                <div className="col-sm-6 col-xs-12">
+                  <img className="focus-item-panel" src={sampleImage}/>
                 </div>
-                <div className="col-sm-8 col-xs-12">
-                  <div>aaaaaaaaaa</div>
+                <div className="col-sm-6 col-xs-12">
+                  <div id="item-detail-panel" className="focus-item-panel" >
+                    <div id="title">Disney T-shirt</div>
+                    <div id="store">store: Lativ</div>
+                    <div id="price">price: $200</div>
+                    <div id="note">note: no</div>
+                  </div>
                 </div>
               </div>
             </div>
 
+
             <div className="block">
-              <div className="title">服飾商店</div>
+              <div className="title">建議穿搭</div>
               <hr ></hr>
+              <div className="recommend-panel radius-border">
+                <div className="combination"></div>
+                <div className="combination"></div>
+                <div className="combination"></div>
+                <div className="combination"></div>
+                <div className="combination"></div>
+              </div>
+
             </div>
 
           </div>
