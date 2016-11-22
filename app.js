6e845281c644981a1be5d5ec65fb4159e9c4591c
Wilson Li <b84330808@gmail.com>
2016-11-23 02:06:39 +0800

    test

--- app.js
+++ app.js
@@ -14,11 +14,13 @@ app.use(sassMiddleware({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(path.join(__dirname, '/public', 'index.html'));
+});
+app.get('/test', function(req, res) {
+    res.sendFile(path.join(__dirname, '/public', 'test.html'));
 });
 //app.get 意思是 如果收到 '/'   
 var server = app.listen(3000, function() {
     console.log('Listening on port 3000');
 });
 
-console.log(path.join(__dirname, 'public'))
